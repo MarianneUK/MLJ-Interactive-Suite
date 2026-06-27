11c3b33995c9b02b6d402ffc5e8ed01f2d5f0ce0
Marianne Johany <marianne.johany@gmail.com>
2026-06-27 17:49:24 +0100

    organise data into folders

--- JS/vocabulary.js
+++ JS/vocabulary.js
@@ -1,43 +1,22 @@
-const words = [
-    {
-        word: "Generous",
-        definition: "Kind and willing to give or share.",
-        level: "B1 Vocabulary"
-    },
-    {
-        word: "Greedy",
-        definition: "Wanting more money, food or power than you need.",
-        level: "B1 Vocabulary"
-    },
-    {
-        word: "Fairness",
-        definition: "The quality of treating people equally and in a reasonable way.",
-        level: "B2 Vocabulary"
-    },
-    {
-        word: "Honest",
-        definition: "Always telling the truth.",
-        level: "A2 Vocabulary"
-    },
-    {
-        word: "Brave",
-        definition: "Showing courage in difficult situations.",
-        level: "B1 Vocabulary"
-    },
-    {
-        word: "Patient",
-        definition: "Able to wait calmly without getting angry.",
-        level: "B1 Vocabulary"
-    }
-];
-
 let previousIndex = -1;
 const wordElement = document.getElementById("word");
 const definitionElement = document.getElementById("definition");
 const levelElement = document.getElementById("level");
 const button = document.getElementById("nextWord");
 
+fetch("data/vocabulary/values.json")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        words = data;
+    });
+
 button.addEventListener("click", function () {
+    if (words.length === 0) {
+    return;
+}
+    
     document.querySelector(".card").classList.add("fade");
 
     setTimeout(function () {
